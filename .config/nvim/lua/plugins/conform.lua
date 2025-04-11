ebc5cf6c342dac28da224b3f09349656928fda6b
Patryk Grabowski <grabowskip@icloud.com>
2025-04-11 21:03:31 +0200

    fix: conform stuff

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,10 +2,21 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      lua = { "stylua" },
-      python = { "ruff", "isort" },
-      javascript = { "biome", "biome-organize-imports" },
-      json = { "biome" },
+      default_format_opts = {
+        timeout_ms = 3000,
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 200,
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff", "isort" },
+        javascript = { "biome", "biome-organize-imports" },
+        typescript = { "biome", "biome-organize-imports" },
+        typescriptreact = { "biome", "biome-organize-imports" },
+        json = { "biome" },
+      },
     },
   },
 }
